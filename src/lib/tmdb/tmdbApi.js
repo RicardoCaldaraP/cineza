2bf2c60887cf3911aa879a7f42746dc9a7a8d7e2
Ricardo Caldara Pinto <ricardo.caldara@uvvnet.com.br>
2025-05-13 11:47:06 -0300

    fix key

--- src/lib/tmdb/tmdbApi.js
+++ src/lib/tmdb/tmdbApi.js
@@ -1,19 +1,21 @@
-
-const TMDB_API_KEY = '84a758e0a08c2b5d41f038f6a44f593c';
-const TMDB_BASE_URL = 'https://api.themoviedb.org/3';
+const TMDB_API_KEY =
+  "eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiI4NGE3NThlMGEwOGMyYjVkNDFmMDM4ZjZhNDRmNTkzYyIsIm5iZiI6MTc0NjQ2NDAxMC40OCwic3ViIjoiNjgxOGVkMGE0ODZhZDY4ZmViNjQwMTYzIiwic2NvcGVzIjpbImFwaV9yZWFkIl0sInZlcnNpb24iOjF9.UbpCJM8JyZdiAWF7NcQLuVSoTkknoPdJ1V8HA9HuQwkf6a44f593c";
+const TMDB_BASE_URL = "https://api.themoviedb.org/3";
 
 export const fetchFromTMDB = async (endpoint, params = {}) => {
   const urlParams = new URLSearchParams({
     api_key: TMDB_API_KEY,
-    language: 'pt-BR',
+    language: "pt-BR",
     ...params,
   });
   const url = `${TMDB_BASE_URL}/${endpoint}?${urlParams}`;
-  
+
   try {
     const response = await fetch(url);
     if (!response.ok) {
-      console.error(`TMDB API Error for ${url}: ${response.status} ${response.statusText}`);
+      console.error(
+        `TMDB API Error for ${url}: ${response.status} ${response.statusText}`
+      );
       const errorData = await response.json().catch(() => null);
       console.error("TMDB Error Details:", errorData);
       throw new Error(`Erro na API TMDB: ${response.statusText}`);
